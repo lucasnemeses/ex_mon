7611fb948f1791c80bd4d1c7a39e48ee99842026
Lucas Meneses <lucasnemeses01@gmail.com>
2022-08-27 20:30:03 -0300

    :sparkles: Criando a ação de cura

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -26,7 +26,7 @@ defmodule ExMon do
 
   defp do_move({:ok, move}) do
     case move do
-      :move_heal -> "esquiva"
+      :move_heal -> Actions.heal()
       move -> Actions.attack(move)
     end
 
